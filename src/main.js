5c5031c49b505732e17cf8ab45b008c950cf72e3
xrx <xrx@xrx.cn>
2021-04-20 09:17:56 +0800

    黑马头条初始化

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//引入全局样式
+import '@/styles/reset.less'
+
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
