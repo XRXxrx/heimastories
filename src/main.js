c984b8dec4196507dde5815ff3c0762a162fb888
xrx <xrx@xrx.cn>
2021-04-20 10:01:02 +0800

    基本登录页面和路由配置

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//引用路由
+import router from '@/router/index'
+
 //引入全局样式
 import '@/styles/reset.less'
 
 Vue.config.productionTip = false
 
 new Vue({
+    //注册路由
+    router,
     render: h => h(App),
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+//模块化
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+//明确安装路由功能
+Vue.use(VueRouter)
+
+//创建路由模块和配置路由
+const router = new VueRouter({
+    //使用toutes进行路由配置
+    routes: [{
+        name: 'login',
+        path: '/login',
+        component: () =>
+            import ('@/views/user/login.vue')
+    }]
+})
+
+//暴露
+export default router
